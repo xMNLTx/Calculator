88ccdda98fd704a361d2296ec8a7e10cd68a424b
MNLT <dspacevgavm@gmail.com>
2024-02-23 11:13:27 +0300

    No

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main(page):
     def btn_click(e):
         greetings.current.controls.append(
             ft.Text(
-                f"Hello, {first_name.current.value} {last_name.current.value}!")
+                f"Привет, {first_name.current.value} {last_name.current.value}!!!")
         )
         first_name.current.value = ""
         last_name.current.value = ""
@@ -18,14 +18,14 @@ def main(page):
         first_name.current.focus()
 
     page.add(
-        ft.TextField(ref=first_name, label="First name", autofocus=True),
-        ft.TextField(ref=last_name, label="Last name"),
-        ft.ElevatedButton("Say hello!", on_click=btn_click),
+        ft.TextField(ref=first_name, label="Введи имя", autofocus=True),
+        ft.TextField(ref=last_name, label="Введи фамилию"),
+        ft.ElevatedButton("Приветствие!", on_click=btn_click),
         ft.Column(ref=greetings),
     )
 
 
-ft.app(target=main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
 
 
 ft.app(main)
